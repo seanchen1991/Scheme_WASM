f8eccac92873db9c79b6de6e2ce905520068f6ad
Sean Chen <skypemaster007@gmail.com>
2019-11-07 21:01:07 -0800

    Add lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,12 +2,15 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::iter::Peekable;
 
+use iter::GentleIterator;
+
 #[derive(Debug, Clone)]
 pub enum Token {
   LParen,
   RParen,
   Symbol(String),
-  Number(f64),
+  Integer(i64),
+  Float(f64),
   Boolean(bool),
   Chr(char),
   Str(Rc<RefCell<String>>)
@@ -16,8 +19,8 @@ pub enum Token {
 impl Token {
   fn get(ch: char) -> Token {
     match ch {
-      "(" => Token::LParen,
-      ")" => Token::RParen,
+      '(' => Token::LParen,
+      ')' => Token::RParen,
       x   => Token::Chr(x)
     }
   }
@@ -33,7 +36,7 @@ impl<I: Iterator<Item=char>> TokenIterator<I> {
   }
 }
 
-impl<I: Iterator<Item=char>> Iterator for TokenIterator {
+impl<I: Iterator<Item=char>> Iterator for TokenIterator<I> {
   type Item = Token;
 
   fn next(&mut self) -> Option<Token> {
@@ -61,14 +64,16 @@ pub fn tokenize<I>(iter: &mut Peekable<I>) -> Vec<Token>
   let mut tokens: Vec<Token> = vec![];
 
   while let Some(x) = tokenize_single(iter) {
-    tokens.push(x)
+    tokens.push(x);
   }
+
+  tokens
 }
 
 fn parse_whitespace<I>(iter: &mut Peekable<I>) -> bool
   where I: Iterator<Item=char>
 {
-  if check_chr(iter, " ") || check_chr(iter, "\n") {
+  if check_chr(iter, ' ') || check_chr(iter, '\n') {
     iter.next();
     true
   } else {
@@ -79,12 +84,84 @@ fn parse_whitespace<I>(iter: &mut Peekable<I>) -> bool
 fn parse_comment<I>(iter: &mut Peekable<I>) -> bool 
   where I: Iterator<Item=char>
 {
-  if check_chr(iter, ";") {
-    iter.take_until(|c| *c != "\n");
+  if check_chr(iter, ';') {
+    iter.take_until(|c| *c != '\n');
     true
   } else {
     false
   }
+}
+
+fn parse_lparen<I>(iter: &mut Peekable<I>) -> Option<Token>
+  where I: Iterator<Item=char>
+{
+  parse_single(iter, '(')
+}
+
+fn parse_rparen<I>(iter: &mut Peekable<I>) -> Option<Token>
+  where I: Iterator<Item=char>
+{
+  parse_single(iter, ')')
+}
+
+fn parse_string<I>(iter: &mut Peekable<I>) -> Option<Token>
+  where I: Iterator<Item=char>
+{
+  if !check_chr(iter, '"') {
+    return None;
+  }
+
+  iter.next();
+  let value = iter.take_until(|c| *c != '"').collect();
+  iter.next();
+  
+  Some(Token::Str(Rc::new(RefCell::new(value))))
+}
+
+fn parse_hash<I>(iter: &mut Peekable<I>) -> Option<Token>
+  where I: Iterator<Item=char>
+{
+  if !check_chr(iter, '#') {
+    return None;
+  }
+
+  iter.next();
+  match iter.next() {
+    Some('t') => Some(Token::Boolean(true)),
+    Some('f') => Some(Token::Boolean(false)),
+    Some(c)   => panic!("Expected #t or #f, got: #{}", c),
+    None      => panic!("Expected char after '#', got none")
+  }
+}
+
+fn parse_symbol<I>(iter: &mut Peekable<I>) -> Option<Token>
+  where I: Iterator<Item=char>
+{
+  if !check(iter, |_| true) {
+    return None;
+  }
+
+  let value: String = iter.take_until(|c| *c != ' ' && *c != ')' && *c != '\n').collect();
+
+  parse_number(&value)
+    .or_else(|| Some(Token::Symbol(value)))
+}
+
+fn parse_number(value: &str) -> Option<Token> {
+  value.parse::<i64>().map(Token::Integer)
+    .or_else(|_| value.parse::<f64>().map(Token::Float))
+    .ok()
+}
+
+fn parse_single<I>(iter: &mut Peekable<I>, chr: char) -> Option<Token>
+  where I: Iterator<Item=char>
+{
+  if !check_chr(iter, chr) {
+    return None;
+  }
+
+  iter.next();
+  Some(Token::get(chr))
 }
 
 fn check<F, I>(iter: &mut Peekable<I>, f: F) -> bool 
